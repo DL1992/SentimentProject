4aa6d97594cbfe5c2e82efad714e2acb2ce5a683
DL1992 <laadan1235@gmail.com>
2020-12-21 17:18:21 +0200

    Initial commit

---
+++ algorithm/sentiment_model.py
@@ -0,0 +1,5 @@
+import pickle
+from sklearn import metrics
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
